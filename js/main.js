23511bade65c1cfa3d9986441873080c1d10f2aa
Nasar-uddin <nasaruddin171@gmail.com>
2020-04-23 21:06:56 +0600

    Home page done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+	var heroCarousel = document.querySelectorAll('.carousel');
+	var instance = M.Carousel.init(heroCarousel[0], {
+		fullWidth: true,
+		indicators: true
+	});
+	var instances = M.Carousel.init(heroCarousel[1], {
+		dist:0,
+	});
+});
